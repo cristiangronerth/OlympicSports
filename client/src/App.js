be9d3590552321e2b5ce1e931191fbbe7779cae2
NazarenoRios <nazarenolrios@gmail.com>
2022-08-23 12:50:33 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-blue-600 text-xl " >
+        Hello world
+      </h1>
+    </div>
+  );
+}
+
+export default App;
