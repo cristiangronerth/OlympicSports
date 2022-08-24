f8d3e93ffc40840359426bb65055a80dd0360e47
NazarenoRios <nazarenolrios@gmail.com>
2022-08-24 11:21:54 -0300

    pagina principal

---
+++ client/src/components/Home/Collections/Collections.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ImgPortada from './ImgPortada'
+import Btn from '../../../common/Btn'
+import { StyledCategorySection, StyledCollections, StyledHeading, StyledSubHeading, Wrapper } from './styledComponents'
+import { motion } from "framer-motion"
+
+function Collections() {
+  return (
+    <Wrapper>
+      <StyledCollections >
+        <StyledHeading>
+          <h1 >
+            <span>
+              OLYMPIC,
+            </span>
+          </h1>
+          <StyledSubHeading>
+            <h1 >
+              <span>
+                SPORTS
+              </span>
+            </h1>
+            <motion.h3 initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+              COLL.
+              <br />
+              2022
+            </motion.h3>
+          </StyledSubHeading>
+        </StyledHeading>
+        <StyledCategorySection>
+          <ImgPortada />
+        </StyledCategorySection>
+        <Btn/>
+      </StyledCollections>
+    </Wrapper>
+  )
+}
+
+export default Collections
