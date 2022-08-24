cb2011ad23de83ef5fa99476760413847b14d2de
francisco <francisco.leyescampoy@gmail.com>
2022-08-24 18:42:25 -0300

    navbar terminada

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import BarDivider from '../components/Home/BarDivider/BarDivider'
-import Categories from '../components/Home/Categories/Categories'
-import Collections from '../components/Home/Collections/Collections'
-import PreInfo from '../components/Home/PreInfo/PreInfo'
-import Testimonials from '../components/Home/Testimonials/Testimonials'
+import React from "react";
+import Navbar from "../common/Navbar";
+import BarDivider from "../components/Home/BarDivider/BarDivider";
+import Categories from "../components/Home/Categories/Categories";
+import Collections from "../components/Home/Collections/Collections";
+import PreInfo from "../components/Home/PreInfo/PreInfo";
+import Testimonials from "../components/Home/Testimonials/Testimonials";
 
 export default function Home() {
-
-
   return (
     <>
-        <Collections/>
-        <BarDivider/>
-        <PreInfo/>
-        <Categories/>
-        <Testimonials/>
+      <Navbar />
+      <Collections />
+      <BarDivider />
+      <PreInfo />
+      <Categories />
+      <Testimonials />
     </>
-  )
+  );
 }
