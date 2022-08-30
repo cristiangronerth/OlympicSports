b569f6a2094fc10b3f62199de926ef93e09dda87
francisco <francisco.leyescampoy@gmail.com>
2022-08-30 17:23:37 -0300

    search products

--- client/src/components/Home/Collections/Collections.jsx
+++ client/src/components/Home/Collections/Collections.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import ImgPortada from './ImgPortada'
-import Btn from '../../../common/Btn'
-import { StyledCategorySection, StyledCollections, StyledHeading, StyledSubHeading, Wrapper } from './styledComponents'
-import { motion } from "framer-motion"
+import ImgPortada from "./ImgPortada";
+import Btn from "../../../common/Btn";
+import {
+  StyledCategorySection,
+  StyledCollections,
+  StyledHeading,
+  StyledSubHeading,
+  Wrapper,
+} from "./styledComponents";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 function Collections() {
   return (
     <Wrapper>
-      <StyledCollections >
+      <StyledCollections>
         <StyledHeading>
-          <h1 >
-            <span>
-              OLYMPIC,
-            </span>
+          <h1>
+            <span>OLYMPIC,</span>
           </h1>
           <StyledSubHeading>
-            <h1 >
-              <span>
-                SPORTS
-              </span>
+            <h1>
+              <span>SPORTS</span>
             </h1>
             <motion.h3 initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
               COLL.
@@ -31,10 +34,12 @@ function Collections() {
         <StyledCategorySection>
           <ImgPortada />
         </StyledCategorySection>
-        <Btn title="EXPLORE" />
+        <Link to="/search">
+          <Btn title="EXPLORE" />
+        </Link>
       </StyledCollections>
     </Wrapper>
-  )
+  );
 }
 
-export default Collections
+export default Collections;
